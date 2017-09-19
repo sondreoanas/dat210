5c23f5e602dc835d288a426ca69a01c012536d7b
MPettersen <mp.markus94@gmail.com>
2017-09-19 13:28:43 +0200

    markus

--- Generic Calendar/back.py
+++ Generic Calendar/back.py
@@ -3,7 +3,10 @@ Python backend
 For communication with DB and python frontend
 Only send valid data to the DB, validate data from frontend
 Retrieve required data from DB when needed and send to frontend
+
+Sist oppdatert: 19.09.17 13:22 av Markus
 """
+
 
 from flask import Flask, render_template, g, request, redirect, url_for, flash, abort, session, send_from_directory
 import mysql.connector
@@ -23,3 +26,10 @@ app.config["DATABASE_HOST"] = "localhost"
 app.config["DEBUG"] = True  # only for development!
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.secret_key = "any random string"
+
+
+def get_db():
+    if not hasattr(g, "_db"):
+        g.db = mysql.connector.connect(host=app.config["DATABASE_HOST"], user=app.config["DATABASE_USER"],
+                                       password=app.config["DATABASE_PASSWORD"], database=app.config["DATABASE_DB"])
+    return g.db
