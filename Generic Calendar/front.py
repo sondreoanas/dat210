9dacaa1a90347de01ed16667829fb1c29db9d841
sondreoanas <sondreoanas@gmail.com>
2017-09-19 13:12:36 +0200

    Update front.py

--- Generic Calendar/front.py
+++ Generic Calendar/front.py
@@ -2,3 +2,16 @@
 #     Sondre J. Oanæs, Front-end Python
 #     Siste oppdateret: 19.09.2017 12:52
 #
+
+
+
+# Login
+
+# Sikkerhet
+
+# Kommunikasjon med Back-end
+
+# Sjekke komunikasjon med front_end
+
+
+# Sjekke alt!
