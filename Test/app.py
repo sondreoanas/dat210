f8868e2530b2e8c04240c6185f2e39199784122e
sondreoanas <sondreoanas@gmail.com>
2017-09-29 14:53:12 +0200

    Update app.py

--- Test/app.py
+++ Test/app.py
@@ -11,8 +11,8 @@ import json
 from flask import Flask, request, redirect, url_for, render_template, flash, session
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/test")
+def test():
     data = {
         'templ':'<h1></h1>'
         'data': {
@@ -22,6 +22,10 @@ def index():
     }
     
     return json.dumps(data)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
