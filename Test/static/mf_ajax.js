c073249ce6c1150934b6fb232c92f93cff40a907
Vipiao <markus@fjellheim.org>
2017-10-02 00:31:12 +0200

    ajax test with python server
    -Added html to python server interface via ajax.
    -Changed how the timeline is added to the html.
    -Fixed inifite loop in timeline if zooming in too quickly.
    -Added in maximum and minimum zoom level between 2 minutes ant 10 years.

---
+++ Test/static/mf_ajax.js
@@ -0,0 +1,66 @@
+window.addEventListener("load", initAjax);
+function mf_AjaxHandler(){
+	this.hasLoadedTimeline = false;
+}
+var mf_ajaxHandler = new mf_AjaxHandler();
+
+function initAjax(){
+	var body = document.getElementsByTagName("body")[0];
+	searchChildren(body);
+}
+function searchChildren(element){
+	if(!findAjaxData(element)){
+		for(var i=0; i<element.children.length; i++){
+			var child = element.children[i];
+			searchChildren(child);
+		}
+	}
+}
+function findAjaxData(element){
+	if(element.dataset.ajax){
+		if(element.dataset.ajax == "/timeline"){ // load "mf_timeline.js"
+			if(!mf_ajaxHandler.hasLoadedTimeline){
+				ajax("/static/mf_timeline.js", function(responseText){
+					eval(responseText);
+					init();
+					addTimeline(element);
+				});
+				mf_ajaxHandler.hasLoadedTimeline = true;
+			}else{
+				addTimeline(element)
+			}
+		}else{ // load in other content
+			ajax(element.dataset.ajax, function(responseText){
+				loadInContent(element, JSON.parse(responseText));
+			});
+		}
+		// loading graphic?
+		element.innerHTML = "loading...";
+		
+		return true;
+	}else{
+		return false;
+	}
+}
+function loadInContent(element, data){
+	//data.template = template(data.data, data.template);
+	element.innerHTML = data.template;
+}
+function ajax(address, callback){
+	var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200){
+				callback(this.responseText);
+			}
+		}
+		xhttp.open("GET", address, true);
+		xhttp.send();
+}
+
+
+
+
+
+
+
+
