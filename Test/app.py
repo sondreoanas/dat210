0127fc4b7341492838a08249b020105021f11edd
sondreoanas <sondreoanas@Sondres-MacBook-Pro.local>
2017-10-02 13:22:53 +0200

    Sondre 02.10.2017

--- Test/app.py
+++ Test/app.py
@@ -5,7 +5,8 @@
     Sist oppdatert: Sondre 02.10.2017
 
 """
-import back_test     #Back-end python script
+#Back-end python script
+import back
 import json
 from flask import Flask, request, redirect, url_for, render_template, flash, session
 app = Flask(__name__)
@@ -13,7 +14,11 @@ app = Flask(__name__)
     
 @app.route("/getHTML")
 def test(html):
+<<<<<<< Updated upstream
     with open('html/' + html , 'r') as f:
+=======
+    with open('html/' + html, 'r') as f:
+>>>>>>> Stashed changes
         template = f.read()
         
     data = {
@@ -22,6 +27,12 @@ def test(html):
     }
     
     return json.dumps(data)
+
+@app.route("/login")
+def login(username, password):
+    data = request.json
+    bolean = back.valid_login(data['username'],data['password'])
+    return json.dumps(bolean)
 
 @app.route("/")
 def index():
