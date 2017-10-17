2ba9636db9116990f46c50e39778fc527a0d4bdb
sondreoanas <sondreoanas@Sondres-MacBook-Pro.local>
2017-10-17 08:31:38 +0200

    Sondre 17.10.2017

--- Generic Calendar/security.py
+++ Generic Calendar/security.py
@@ -11,46 +11,38 @@ import os
 from werkzeug.security import pbkdf2_hex
 import re
 
-import back
-
 
 # Password Check:
 # Checks if a password is valid
 
-def password_check(username,user_password,salt):
-    #salt = from database
-    client_password = pbkdf2_hex(user_password, salt, iterations=50000, keylen=None, hashfunc=None) # Default hashfunc SHA-256
-    return back.valid_login(username,client_password)
+def check_password(user_password, client_password, salt):
+    user_password = pbkdf2_hex(user_password,salt,iterations=50000, keylen=None, hashfunc=None) # Default hashfunc SHA-256
+    if user_password == client_password: return True
+    else: return False
 
 # Create Password:
 # Creates a password using a unique salt and a hashing algorithm
 
-def create_password(username,user_password, name):
+def create_password(user_password):
     salt = os.urandom(10)
     client_password = pbkdf2_hex(user_password, salt, iterations=50000, keylen=None, hashfunc=None)
-    return back.valid_register_user(username,client_password,name)
-
-def password_check_test(user_password, client_password, salt):
-    #salt = from database
-    if client_password == pbkdf2_hex(user_password, salt, iterations=50000, keylen=None, hashfunc=None): # Default hashfunc SHA-256
-        return True
-    else: return False
-
-
-def create_password_test(user_password):
-    salt = os.urandom(10)
-    client_password = pbkdf2_hex(user_password, salt, iterations=50000, keylen=None, hashfunc=None)
-    return client_password
-
+    return [client_password, salt]
 
 # Email Check:
 # Check if email is in the email format
 
 def check_email(email):
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
+    # Regex source: http://stackoverflow.com/questions/8022530/python-check-for-valid-email-address
     if EMAIL_REGEX.match(email): return True
     else: return False
 
+def check_int( object, length, type):
+    if isinstance(object,type):
+        if len(str(object)) == length: return True
+        elif length is None: return True
+        else: return False
+    return False
 
 
 # Inputtype Check:
@@ -61,8 +53,5 @@ INJECTIONS = ['<script>']
 def check_input(input):
     # Injection check:
     for injection in INJECTIONS:
-        if input in injection: return False
-        else: return True
-
-
-
+        if input in injection: return NameError
+        return input.strip('"\'')
